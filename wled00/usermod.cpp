14a6e9d008f1005b4a7b66dc1310b39eca3a6327
Filip Viduna <viduna.f@gmail.com>
2021-06-28 21:56:31 +0200

    DHT22 MQTT

--- wled00/usermod.cpp
+++ wled00/usermod.cpp
@@ -10,20 +10,45 @@
 
 //Use userVar0 and userVar1 (API calls &U0=,&U1=, uint16_t)
 
+#include <DHT.h>
+
+#define humidity_topic "wled/shelf/hum"
+#define temperature_topic "wled/shelf/temp"
+
+#define DHTTYPE DHT22
+#define DHTPIN  4
+
+DHT dht(DHTPIN, DHTTYPE);
+
 //gets called once at boot. Do all initialization that doesn't depend on network here
 void userSetup()
 {
-  
+  dht.begin();
 }
 
 //gets called every time WiFi is (re-)connected. Initialize own network interfaces here
 void userConnected()
 {
-
 }
+
+long lastMsg = 0;
+float temp = 0.0;
+float hum = 0.0;
 
 //loop. You can use "if (WLED_CONNECTED)" to check for successful connection
 void userLoop()
 {
-  
+    if (mqtt != nullptr && mqtt->connected())
+    {
+        long now = millis();
+        if (now - lastMsg > 300000) {
+            lastMsg = now;
+
+            float temp = dht.readTemperature();
+            float hum = dht.readHumidity();
+
+            mqtt->publish(temperature_topic, 0, true, String(temp).c_str());
+            mqtt->publish(humidity_topic, 0, true, String(hum).c_str());
+        }
+    }
 }
